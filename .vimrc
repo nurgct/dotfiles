a7528daf6a6acb608fe112e250e3f24d7c71d64e
Leon Goldner Cohen Tzedek <leon@goldnercohentzedek.com>
2020-10-12 16:30:19 -0400

    redo

---
+++ .vimrc
@@ -0,0 +1,23 @@
+colorscheme gruvbox " a colorscheme
+set background=dark
+
+set tabstop=4 "sets spaces seen per tab to 4
+set softtabstop=4 "how many tabs will be seen when I hit tab
+
+
+
+"UI config
+set number "shows line numbers
+syntax enable " enables syntax highlighting
+
+set cursorline "highlights the current line
+
+set showmatch "highlight matching brackets and stuff
+
+set incsearch "search as characters are entered
+set hlsearch "highlight matches
+
+"file search config 
+"search down into sub folders (recursive searc)
+set path+=** "provides tab-completion for all file-related tasks
+set wildmenu "display all matching files when using tab-complete
---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,48 @@
+         -- Imports.
+    import XMonad
+    import XMonad.Hooks.DynamicLog
+    import XMonad.Hooks.ManageDocks
+    import XMonad.Util.EZConfig(additionalKeys)
+    import System.IO
+    import Graphics.X11.ExtraTypes.XF86
+    import XMonad.Hooks.FadeInactive 
+    -- The main function --
+    main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+ 
+    -- Command to launch the bar --
+    myBar = "xmobar"
+ 
+     -- Custom PP, configure it as you like. It determines what is being written to the bar.
+    myPP = xmobarPP { ppCurrent = xmobarColor "#ff0000" "" . wrap " " " " }
+ 
+    --Custom Terminal Emulutor--
+    myTerminal = "sakura -e /bind/bash"
+ 
+    -- Key binding to toggle the gap for the bar.
+    toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+ 
+    --Works Spaces--
+    myWorkspaces = ["1:code","2:web","3:im","4:game","5:misc"]
+ 
+    --Hooks: Sending windows to the proper workspaces--
+    myManageHook = composeAll
+        [ className =? "Firefox" --> doShift "web",
+          className =? "Pidgin" --> doShift "im",
+          className =? "HexChat" --> doShift "im",
+          className =? "Steam" --> doShift "game",
+          className =? "Quadrapassel" --> doShift "game"
+        ]      
+ 
+    -- Main configuration, override the defaults to your liking.
+    myConfig = defaultConfig{ 
+                            modMask = mod4Mask,
+                            workspaces = myWorkspaces,
+                            manageHook = myManageHook,
+                            terminal = myTerminal
+                            } `additionalKeys` 
+								[ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock"),
+                                  ((0, 0x1008FF11), spawn "amixer -q sset Master 2%-"),
+                                  ((0, 0x1008FF13), spawn "amixer -q sset Master 2%+"),
+                                  ((0, 0x1008FF02), spawn "lux -a 10%"), --increases the monitor brightness--
+                                  ((0, 0x1008FF03), spawn "lux -s 10%") --decrease the monitor brightness--   
+                                 ]
